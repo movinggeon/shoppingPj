dab29210ad3cc33b3e7c75ed3745331c142b8d8b
jhkwag970 <jhkwag970@gmail.com>
2022-03-14 02:46:03 +0900

    security path sorted maybe..

--- src/main/java/com/group6/shopping/admin/controllers/AdminController.java
+++ src/main/java/com/group6/shopping/admin/controllers/AdminController.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/admin")
 @Controller
 public class AdminController {
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/qwe/admin")
 	public String accessAdmin() {
 
 		return "/admin/index";
+	}
+
+	@GetMapping(value = "/qwe/member")
+	public String accessMember() {
+
+		return "/member/index";
 	}
 
 
--- src/main/webapp/WEB-INF/views/everyone/accessDenied.jsp
+++ src/main/webapp/WEB-INF/views/everyone/accessDenied.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: jhkwa
-  Date: 2022-03-08
-  Time: 오전 2:45
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
