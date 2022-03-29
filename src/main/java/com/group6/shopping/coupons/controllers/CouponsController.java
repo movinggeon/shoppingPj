52a5be8d10cc794a9ebb6d5238fa19f09964951f
Dong Geon Lee <wotn2517@naver.com>
2022-03-29 21:49:55 +0900

    신규회원 쿠폰 지급

--- src/main/java/com/group6/shopping/coupons/controllers/CouponsController.java
+++ src/main/java/com/group6/shopping/coupons/controllers/CouponsController.java
@@ -1,9 +1,29 @@
 package com.group6.shopping.coupons.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.group6.shopping.coupons.services.CouponsService;
+import com.group6.shopping.security.CustomMemDetails;
+
 @Controller
-@RequestMapping("/coupons")
+@RequestMapping("/members")
 public class CouponsController {
+	
+	@Autowired
+	private CouponsService couponsService;
+	
+	@RequestMapping(value = "/coupons")
+	public String couponList(HttpSession session, Model model) throws Exception {
+		
+		CustomMemDetails cs = (CustomMemDetails)session.getAttribute("user");
+		
+		
+		
+		return "members/mypage/couponList";
+	}
 }
--- src/main/java/com/group6/shopping/coupons/services/CouponsServiceImpl.java
+++ src/main/java/com/group6/shopping/coupons/services/CouponsServiceImpl.java
@@ -1,12 +1,14 @@
 package com.group6.shopping.coupons.services;
 
-import com.group6.shopping.batis.CouponsDAO;
-import com.group6.shopping.coupons.vo.CouponsVO;
-import com.group6.shopping.security.CustomMemDetails;
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.group6.shopping.batis.CouponsDAO;
+import com.group6.shopping.coupons.vo.CouponsVO;
+import com.group6.shopping.security.CustomMemDetails;
 
 @Service
 public class CouponsServiceImpl implements CouponsService{
@@ -22,5 +24,15 @@ public class CouponsServiceImpl implements CouponsService{
     @Override
     public int usedCoupon(CouponsVO couponsVO) throws Exception {
         return couponsDAO.usedCoupon(couponsVO);
+    }
+    
+    @Override
+    public int countCoupon(String memId) throws Exception {
+    	return couponsDAO.countCoupon(memId);
+    }
+    
+    @Override
+    public void insertCoupon(HashMap<String, Object> param) throws Exception {
+    	couponsDAO.insertCoupon(param);
     }
 }
