a7f1e6b1b930a9ef82e52d30eef118a090533c52
Jiyul Kim <khy3640@naver.com>
2022-04-05 16:04:26 +0900

    좋아요 만드는중

--- src/main/java/com/group6/shopping/likes/vo/LikesVO.java
+++ src/main/java/com/group6/shopping/likes/vo/LikesVO.java
@@ -1,13 +1,16 @@
 package com.group6.shopping.likes.vo;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 //필드 생성후 noargs 주석 제거
-@Getter@Setter@AllArgsConstructor
-//@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LikesVO {
+    private int like_id;
+    private int spec_id;
+    private String mem_id;
 }
