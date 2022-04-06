d0d264d623c64107d0c69d45d30841c04472cb25
Jiyul Kim <khy3640@naver.com>
2022-04-06 23:02:35 +0900

    좋아요 리스트추가

--- src/main/java/com/group6/shopping/likes/controllers/LikesController.java
+++ src/main/java/com/group6/shopping/likes/controllers/LikesController.java
@@ -2,7 +2,10 @@ package com.group6.shopping.likes.controllers;
 
 import com.group6.shopping.likes.services.LikesService;
 import com.group6.shopping.likes.vo.LikesVO;
+import com.group6.shopping.models.services.ModelsService;
 import com.group6.shopping.security.CustomMemDetails;
+import com.group6.shopping.specifications.services.SpecService;
+import com.group6.shopping.specifications.vo.SpecDisplayVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,20 +16,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-/*@RequestMapping("/likes")*/
-@RequestMapping("/members")
+@RequestMapping("/likes")
+
 public class LikesController {
     @Autowired
     private LikesService likesService;
 
-    @RequestMapping(value = "/likeslist")
+    @RequestMapping(value = "/members/likeslist")
     public String likeList(HttpSession session, Model model) throws Exception {
-        List<LikesVO> likeList=new ArrayList<LikesVO>();
         CustomMemDetails cs = (CustomMemDetails)session.getAttribute("user");
         String memId = cs.getMem_id();
-        System.out.println("접속한유저" + memId);
-        likeList=likesService.getAlllikes(memId);
-        model.addAttribute("likeList",likeList);
+        List<LikesVO> likesVOList = likesService.LikeList(memId);
+        model.addAttribute("likeList",likesVOList);
         return "members/mypage/likeList";
     }
 }
