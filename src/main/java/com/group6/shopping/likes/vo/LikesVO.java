c7d0dc3bba62a210cded851255b74d8a3c741408
Jiyul Kim <khy3640@naver.com>
2022-04-06 22:59:07 +0900

    좋아요 리스트 추가

--- src/main/java/com/group6/shopping/likes/vo/LikesVO.java
+++ src/main/java/com/group6/shopping/likes/vo/LikesVO.java
@@ -1,6 +1,8 @@
 package com.group6.shopping.likes.vo;
 
 
+import com.group6.shopping.models.vo.ModelsVO;
+import com.group6.shopping.products.vo.ProductsVO;
 import lombok.*;
 
 //필드 생성후 noargs 주석 제거
@@ -13,4 +15,8 @@ public class LikesVO {
     private int like_id;
     private int spec_id;
     private String mem_id;
+    private int model_id;
+
+    private ModelsVO modelsVO;
+    private ProductsVO productsVO;
 }
